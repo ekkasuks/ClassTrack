5c6c918b19e91eef3374d96c7e324f2164fa03b0
ekkasuks <ekkasuks@gmail.com>
2026-04-28 10:32:01 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,7 @@
 
 const CONFIG = {
   /** @type {string} URL ของ Google Apps Script Web App */
-  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbxGB1flLEV7M9pD4MSaf5shRIL5hfNZEycsue9Bje2aNBau4hWuWTr6DgWvZbZDxujn/exec',
+  API_BASE_URL: 'https://script.google.com/macros/s/AKfycbwkTLIkzpSkpSxkko8sCdyEkZ8eX9yJ39lwrYCyG-xlTRxtEwAwzhHCGXpf5X4t6KKe/exec',
 
   /** @type {string} Google OAuth Client ID จาก Google Cloud Console */
   GOOGLE_CLIENT_ID: '175501609129-q2h9ug1cjbs0ov1um9oa4hdr4pvr4tdb.apps.googleusercontent.com',
